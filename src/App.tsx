bb98a2481d8ef6dc40abe98775d70a9153ee0738
Jacksonyaaang <92828055+Jacksonyaaang@users.noreply.github.com>
2023-09-19 16:14:19 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,7 @@ import { fetchTracks } from './lib/fetchTracks';
 import { SavedTrack } from 'spotify-types';
 
 
-const trackUrls = [
-  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
-  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
-  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
-  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
-  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
-];
+
 
 const App = () => {
   const [trackIndex, setTrackIndex] = useState(0);
@@ -22,14 +16,26 @@ const App = () => {
   const initi = () =>{
     setTrackIndex(0)
   }
-  const { data: tracks } = useQuery({
+  const { data: tracks } = useQuery<SavedTrack[]>({
     queryKey: ['tracks'],
     queryFn: fetchTracks
   });
   console.log({ data: tracks })
   console.log("nbr d'éléments qu'il contient")
   console.log({data : tracks}.data.length)
-  
+  const tracksFav = {data : tracks}?.data
+
+
+  const trackUrls = [
+    tracksFav[0].track.preview_url,
+    tracksFav[1].track.preview_url,
+    tracksFav[2].track.preview_url,
+    'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+    'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+    'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+    'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+    'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+  ];
   return (
     <div className="App">
       <header className="App-header">
